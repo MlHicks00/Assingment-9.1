4367022b5e696c27dcf66e8a9d86c9c623b97f39
MlHicks00 <110294375+MlHicks00@users.noreply.github.com>
2022-08-07 23:03:53 -0400

    Add files via upload
    having trouble finding directory from user input

---
+++ Directory_processing.py
@@ -0,0 +1,38 @@
+# import the os library
+import os
+
+def main():
+
+  # User input
+  directory = input("Enter the directory to save your file: ")
+  file_name = input("Enter the file name: ")
+  user_name = input("Enter your name: ")
+  address = input("Enter your address: ")
+  phone_number = input("Enter your phone number: ")
+ 
+      
+  if os.path.isdir(directory):
+  
+  #writing and creating the file entered by user
+    with open(directory, "w") as written_file:
+      written_file.write(file_name + "," +user_name + "," + address + "," + phone_number)
+
+    #make sure to close the file
+      written_file.close()
+  
+    #display the file contents to the user for validation purposes
+    print("The file contents are: ")
+
+    #reading written file
+    with open(directory,"r") as written_file:
+
+    #displaying the file written
+      data = written_file.read()
+      print(data)
+   
+      # close the file
+      written_file.close()
+  else: 
+    print("Error, Directory does not exist")
+
+main()
